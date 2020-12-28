ae2e7d5541d73ec4d033962f383b11988bf9073e
unavailable-sudo <76682714+unavailable-sudo@users.noreply.github.com>
2020-12-28 16:03:06 +0700

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,41 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"log_in")
+                .setDirtyMark(false)
+                .setLeft("22.857142857142858em")
+                .setTop("31.238095238095237em")
+                .setCaption("Log In")
+                .setImageClass("xui-icon-xui")
+                .onClick("_log_in_onclick")
+                .setCustomStyle({
+                    "KEY":{
+                        "color":"#0000FF"
+                    },
+                    "CAPTION":{
+                    }
+                })
+                .setCustomClass({
+                    "CAPTION":"Log In"
+                })
+            );
+            
+            append(
+                xui.create("xui.UI.Image")
+                .setHost(host,"discord")
+                .setLeft("0.7619047619047619em")
+                .setTop("0em")
+                .setSrc("{xui.ini.img_pic}")
+                .setCustomStyle({
+                    "KEY":{
+                        "color":"#8A2BE2"
+                    }
+                })
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
@@ -28,6 +62,17 @@ xui.Class('App', 'xui.Module',{
         customAppend : function(parent, subId, left, top){
             // "return false" will cause all the internal UI controls will be added to the parent panel
             return false;
+        },
+        /**
+         * Fired when user click it
+         * @method onClick [xui.UI.Button event]
+         * @param {xui.UIProfile.} profile  The current control's profile object
+         * @param {Event} e , Dom event object
+         * @param {Element.xui} src  id or Dom Element
+         * @param {} value  Object
+        */
+        _log_in_onclick:function(profile, e, src, value){
+            var ns = this, uictrl = profile.boxing();
         }
         /*,
         // To determine how properties affects this module
